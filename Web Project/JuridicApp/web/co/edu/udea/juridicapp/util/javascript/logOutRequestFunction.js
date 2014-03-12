5aa29fdc1715f68b1fec74e92cf0773e80c51124
Neiber Padierna Pérez <npadierna@gmail.com>
2014-03-11 19:19:27 -0500

    Improving some views in XHTML web views. Creating new methods for implementation some operations.

--- Web Project/JuridicApp/web/co/edu/udea/juridicapp/util/javascript/logOutRequestFunction.js
+++ Web Project/JuridicApp/web/co/edu/udea/juridicapp/util/javascript/logOutRequestFunction.js
@@ -1,6 +1,6 @@
 function handleLogoutRequest(xhr, status, args) {
-   // if (!args.loggedIn) {
+   if (!args.loggedIn) {
         location.href = '/Juridicapp/faces/co/edu/udea/juridicapp/web/work/list/worklist.xhtml';
-   // }
+   }
 }
 
